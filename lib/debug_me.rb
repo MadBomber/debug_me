378101b8e69c649ddb3f20e559619d00ba6bb288
Dewayne VanHoozer <dvanhoozer@gmail.com>
2014-10-02 09:19:08 -0500

    RuboCop auto-corrected stuff

--- lib/debug_me.rb
+++ lib/debug_me.rb
@@ -1,27 +1,25 @@
 require 'pp'
-require "debug_me/version"
+require 'debug_me/version'
 
 module DebugMe
-
-  def debug_me( options={}, &block )
-
+  def debug_me(options = {}, &block)
     default_options = {
-      :tag    => 'DEBUG:',  # A tag to prepend to each output line
-      :time   => true,      # Include a time-stamp in front of the tag
-      :header => true,      # Print a header string before printing the variables
-      :ivar   => true,      # Include instance variables in the output
-      :cvar   => true,      # Include class variables in the output
-      :file   => $stdout    # The output file
+      tag: 'DEBUG:',  # A tag to prepend to each output line
+      time: true,      # Include a time-stamp in front of the tag
+      header: true,      # Print a header string before printing the variables
+      ivar: true,      # Include instance variables in the output
+      cvar: true,      # Include class variables in the output
+      file: $stdout    # The output file
     }
 
     if 'Hash' == options.class.to_s
       options = default_options.merge(options)
     else
-      options = default_options.merge({:tag => options})
+      options = default_options.merge(tag: options)
     end
 
     f = options[:file]
-    s = ""
+    s = ''
     s += "#{sprintf('%010.6f', Time.now.to_f)} " if options[:time]
     s += " #{options[:tag]}"
     wf = caller # where_from under 1.8.6 its a stack trace array under 1.8.7 is a string
@@ -31,30 +29,28 @@ module DebugMe
 
     if block_given?
 
-      block_value = [ block.call ].flatten.compact
+      block_value = [block.call].flatten.compact
 
       if block_value.empty?
         block_value = eval('local_variables', block.binding)
-        block_value += [ eval('instance_variables', block.binding) ] if options[:ivar]
-        block_value += [ self.class.send('class_variables') ] if options[:cvar]
+        block_value += [eval('instance_variables', block.binding)] if options[:ivar]
+        block_value += [self.class.send('class_variables')] if options[:cvar]
         block_value = block_value.flatten.compact
       else
-        block_value.map! { |v| v.to_s }
+        block_value.map!(&:to_s)
       end
 
       block_value.each do |v|
         ev = eval(v, block.binding)
-        f.puts "#{s} #{v} -=> #{pp ev}"  #.pretty_inspect}"
+        f.puts "#{s} #{v} -=> #{pp ev}"  # .pretty_inspect}"
       end
 
     end ## if block_given?
 
     f.flush
-
   end ## def debug_me( options={}, &block )
 
-# def log_me(msg, opts={})
-#   debug_me({:tag => msg, :header => false}.merge(opts))
-# end
-
+  # def log_me(msg, opts={})
+  #   debug_me({:tag => msg, :header => false}.merge(opts))
+  # end
 end # module DebugMe
--- lib/debug_me/version.rb
+++ lib/debug_me/version.rb
@@ -1,3 +1,3 @@
 module DebugMe
-  VERSION = "1.0.0"
+  VERSION = '1.0.0'
 end
