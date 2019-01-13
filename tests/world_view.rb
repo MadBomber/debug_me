2bb592f1f4a565c19ab17f42bd9bbcef86ad7ea1
Dewayne VanHoozer <dvanhoozer@gmail.com>
2019-01-13 15:50:42 -0600

    minor format tweeks

--- tests/world_view.rb
+++ tests/world_view.rb
@@ -3,20 +3,23 @@ class WorldView
   A = 1
   B = 2
   C = 3
+
   def initialize
-    @a,@b,@c = 1,2,3
-    @@d, @@e, @@f = 4,5,6
+    @a,@b,@c       = 1,2,3
+    @@d, @@e, @@f   = 4,5,6
   end
+
   def everything
     debug_me(
-      file:nil,
-      header:true,
-      lvar: true,
-      ivar: true,
-      cvar: true,
+      file:   nil,
+      header: true,
+      lvar:   true,
+      ivar:   true,
+      cvar:   true,
       cconst: true
     ){}
   end
+
   def my_constants
     debug_me(
       file:nil,
@@ -26,6 +29,5 @@ class WorldView
       cvar: false,
       cconst: true
     ){}
-
   end
 end # class WorldView
