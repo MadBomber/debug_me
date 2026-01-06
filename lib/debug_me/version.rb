c94e70d8ae219c823973c15a0260a64d77974f4b
Dewayne VanHoozer <dvanhoozer@gmail.com>
2026-01-06 17:11:32 -0600

    bumped version

--- lib/debug_me/version.rb
+++ lib/debug_me/version.rb
@@ -4,5 +4,5 @@ module DebugMe
   # to get direct access to the debug_me method without
   # polluting your code with an object name that is so
   # common.
-  DEBUG_ME_VERSION = '1.1.3'
+  DEBUG_ME_VERSION = '1.1.4'
 end
